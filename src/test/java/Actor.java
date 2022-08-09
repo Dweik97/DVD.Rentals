c09e2fe06ca5f0764040b9ad42e9f11020cff93f
Dweik97 <mdweik97@gmail.com>
2022-08-09 14:44:49 +0100

    09 Aug (04)

--- src/test/java/Actor.java
+++ src/test/java/Actor.java
@@ -1,11 +1,10 @@
-import DVD_Store.Models.Filmography.Actor;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class actor {
+public class Actor {
     private int actorId;
     //Attributes
     private String firstName;
@@ -13,26 +12,26 @@ public class actor {
 
     @Test
     public void test_getFirstName() {
-        Actor testActor = new Actor("First", "Last", new Date());
+        DVD_Store.Models.Filmography.Actor testActor = new DVD_Store.Models.Filmography.Actor("First", "Last", new Date());
         assertEquals("First", testActor.getFirst_name(), "Test failed.");
     }
 
     @Test
     public void test_setFirstName() {
-        Actor testActor = new Actor("First", "Last", new Date());
+        DVD_Store.Models.Filmography.Actor testActor = new DVD_Store.Models.Filmography.Actor("First", "Last", new Date());
         testActor.setFirst_name("Last");
         assertEquals("Last", testActor.getFirst_name(), "Test failed.");
     }
 
     @Test
     public void test_getLastName() {
-        Actor testActor = new Actor("First", "Last", new Date());
+        DVD_Store.Models.Filmography.Actor testActor = new DVD_Store.Models.Filmography.Actor("First", "Last", new Date());
         assertEquals("Last", testActor.getLast_name(), "Test failed.");
     }
 
     @Test
     public void test_setLastName() {
-        Actor testActor = new Actor("First", "Last", new Date());
+        DVD_Store.Models.Filmography.Actor testActor = new DVD_Store.Models.Filmography.Actor("First", "Last", new Date());
         testActor.setLast_name("Last");
         assertEquals("Last", testActor.getLast_name(), "Test failed.");
     }
